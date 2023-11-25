62b528de5ac3b661265e50e14cc772a6f0df2a0a
Nikita Kozlov <kns@kns_pc>
2023-11-26 01:53:12 +0300

    module4-task1

---
+++ frontend/src/modules/constructor/DoughComp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="content__dough">
+    <SheetCard class="dough">
+      <template #title>Выберите тесто</template>
+      <label
+        v-for="dough in normalizedDoughs"
+        :key="dough.id"
+        class="dough__input"
+        :class="`dough__input--${dough.doughSize}`"
+      >
+        <input
+          type="radio"
+          name="dought"
+          :value="dough.doughSize"
+          class="visually-hidden"
+          :checked="dough.doughSize === modelValue"
+          @input="emit('update:modelValue', $event.target.value)"
+        />
+        <b>{{ dough.name }}</b>
+        <span>{{ dough.description }}</span>
+      </label>
+    </SheetCard>
+  </div>
+</template>
+
+<script setup>
+import { SheetCard } from "../../common/components";
+defineProps({
+  normalizedDoughs: {
+    type: Object,
+    required: true,
+  },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+</style>
---
+++ frontend/src/modules/constructor/SizeComp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="content__diameter">
+    <SheetCard class="diameter">
+      <template #title>Выберите размер</template>
+      <label
+        v-for="size in normalizedSizes"
+        :key="size.id"
+        class="diameter__input"
+        :class="`diameter__input--${size.size}`"
+      >
+        <input
+          type="radio"
+          name="diameter"
+          :value="size.size"
+          class="visually-hidden"
+          :checked="size.size === modelValue"
+          @input="emit('update:modelValue', $event.target.value)"
+        />
+        <span>{{ size.name }}</span>
+      </label>
+    </SheetCard>
+  </div>
+</template>
+
+<script setup>
+import { SheetCard } from "../../common/components";
+defineProps({
+  normalizedSizes: { type: Object, required: true },
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+const emit = defineEmits(["update:modelValue"]);
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/app.scss";
+</style>
